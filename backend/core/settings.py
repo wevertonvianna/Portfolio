055bb7a3dd21204238af45e77e5c831442037f81
weverton vianna <wevertonferreiravmax@gmail.com>
2026-03-07 09:57:16 -0300

    adicionado gunicorn e mudando a posição do import os

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +27,7 @@ DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
-import os
+
 # Application definition
 
 INSTALLED_APPS = [
